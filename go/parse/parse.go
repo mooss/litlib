a362d832d99c99c5a665240429b34fb732379df7
mooss <mooss@protonmail.com>
2022-08-07 18:11:18 +0200

    Move functional tools into their own file

--- go/parse/parse.go
+++ go/parse/parse.go
@@ -5,45 +5,6 @@ import (
 	"strings"
 )
 
-////////////////////////
-// Functional helpers //
-////////////////////////
-
-// Pred is a predicate for type T.
-type Pred[T any] func(T) bool
-
-// and combines the given predicates into one, using the and logical operator.
-func and[T any](preds ...Pred[T]) Pred[T] {
-	return func(t T) bool {
-		for _, p := range preds {
-			if !p(t) {
-				return false
-			}
-		}
-		return true
-	}
-}
-
-// nor combines the given predicates into one, using the nor logical operator.
-func nor[T any](preds ...Pred[T]) Pred[T] {
-	return func(t T) bool {
-		for _, p := range preds {
-			if p(t) {
-				return false
-			}
-		}
-		return true
-	}
-}
-
-func Map[T, U any](fun func(T) U, source []T) []U {
-	res := make([]U, len(source))
-	for i, el := range source {
-		res[i] = fun(el)
-	}
-	return res
-}
-
 //////////////
 // Elements //
 //////////////
